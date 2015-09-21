fe251cec18ced2591b5485c638eabcb163c8deb2
Crysler <cyrenoc@gmail.com>
2015-09-21 19:13:31 -0430

    nuevo achivo limpiador3

--- scripts/conversion2limpio4.py
+++ scripts/conversion2limpio4.py
@@ -12,8 +12,14 @@ for index,row in enumerate(reader):
     #print 'Persona: ' + str(index + 1)
     #print '------------'
     #con .join creo el array como una cadena larga
-        row2=""
-    if not (( 'Crime ID' in " ".join(row)) or ( 'Reported by' in " ".join(row))):
+    row2=""
+	#descarto que en el campo tipo de crimenes se encuentren ciudades tambien 	 
+    row2=""
+    crimen=row[9]
+    crimen=crimen.split(' ')[0]
+    ciudadesSolapadas={"Blaby","Charnwood","Bassetlaw","Worthing","Hillingdon"}
+
+    if not (( 'Crime ID' in " ".join(row)) or ( 'Reported by' in " ".join(row))) and not ( crimen in ciudadesSolapadas):
         #reemplazo ;\r\n con \r\n
         if (';\r\n' in " ".join(row)):
             #para guardar en row debe ser iterada desde row2 por medio de un split
@@ -27,7 +33,7 @@ for index,row in enumerate(reader):
             row2=",".join(row).replace(';', ',')
             row=row2.split(',')
         #Crime ID lo transforme en String y se omite para evitar embasuramiento
-        print  '{'+'"Month":'+'"'+row[1]+'"'+','+ '"Reported_by":'+'"'+row[2]+'"'+','+'"Falls_withi$
+        print  '{'+'"Month":'+'"'+row[1]+'"'+','+ '"Reported_by":'+'"'+row[2]+'"'+','+'"Falls_withiin":'+'"'+ row[3]+'"'+','+'"Longitude":'+ row[4]+','+'"Latitude":'+row[5]+','+'"Location":'+'"'+ row[6]+'"'+','+'"LSOA_code":'+'"'+row[7]+'"'+','+'"LSOA_name":'+'"'+row[8]+'"'+','+'"Crime_type":'+'"'+ row[9]+'"'+','+'"Last_outcome_category":'+'"'+row[10]+'"'+','+'"Context":'+'"'+row[11]+'"'+'}'
         print '\n'
         #if index >100:
         #   break
