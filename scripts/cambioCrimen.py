b00d2b9e25d7d111f0f3d4ce054e7c50f6720b32
Crysler <cyrenoc@gmail.com>
2015-12-08 15:54:36 -0430

    version 7

--- scripts/cambioCrimen.py
+++ scripts/cambioCrimen.py
@@ -7,7 +7,7 @@ from datetime import datetime, date, time, timedelta
 import calendar
 # -*- coding: utf-8
 #Variable para la ruta al directorio
-path = '/s3mnt3/procesed/' 
+path = 'todos-crimenes6.json' 
 #Lista vacia para incluir los archivos
 lstFiles = []
 #lista vacia para incluir directorio y archivo 
@@ -15,51 +15,58 @@ lstFiles2 = []
 #Lista con todos los ficheros del directorio
 lstDir = os.walk(path)   #os.walk()Lista directorios y ficheros
 #definiendo una funcion a llamar luego
+
 def creandobigfile(nombreFusionar):
     a = datetime.now()
     print 'anadiendo: '+nombreFusionar 
     #creo el mega archivo en el dir data-britanica-crimenes
     Acsv=open(nombreFusionar, 'rb')
-    reader = csv.reader(Acsv)
-    f = open ("todos-crimenes-tipo.csv", "w")
-    for index,row in enumerate(reader):
-        #print str(row)
-		if("Anti-social behaviour" in row[9]):
-		    row[9]=1
-		if("Bicycle theft" in row[9]):
-		    row[9]=2
-		if("Burglary" in row[9]):
-		    row[9]=3
-		if("Criminal damage and arson" in row[9]):
-		    row[9]=4
-		if("Drugs" in row[9]):
-		    row[9]=5
-		if("Other crime" in row[9]):
-		    row[9]=6
-		if("Other theft" in row[9]):
-		    row[9]=7
-        if("Possession of weapons" in row[9]):
-		    row[9]=8 
-        if("Public disorder and weapons" in row[9]):
-		    row[9]=9
-        if("Public order" in row[9]):
-		    row[9]=10
-        if("Robbery" in row[9]):
-		    row[9]=11
-        if("Shoplifting" in row[9]):
-		    row[9]=12
-        if("Theft from the person" in row[9]):
-		    row[9]=13
-        if("Vehicle crime" in row[9]):
-		    row[9]=14
-        if("Violence and sexual offences" in row[9]):
-		    row[9]=15
-        if("Violent crime" in row[9]):
-		    row[9]=16
+    #reader = csv.reader(Acsv)
+    #reader = (Acsv)
+    f = open ("todos-crimenes-tipo.json", "w")
+    linea=Acsv.readline()
+    while linea!="":   
+        linea=Acsv.readline()
+             #print str(row)
+        if("Anti-social behaviour" in linea):
+            linea=linea.replace('"Anti-social behaviour"', '1')
+        if("Bicycle theft" in linea):
+            linea=linea.replace('"Bicycle theft"', '2')
+        if("Burglary" in linea):
+            linea=linea.replace('"Burglary"', '3')
+        if("Criminal damage and arson" in linea):
+            linea=linea.replace('"Criminal damage and arson"', '4')
+        if("Drugs" in linea):
+            linea=linea.replace('"Drugs"', '5')
+        if("Other crime" in linea):
+            linea=linea.replace('"Other crime"', '6')
+        if("Other theft" in linea):
+            linea=linea.replace('"Other theft"', '7')
+        if("Possession of weapons" in linea):
+            linea=linea.replace('"Possession of weapons"', '8')
+        if("Public disorder and weapons" in linea):
+            linea=linea.replace('"Public disorder and weapons"', '9')
+        if("Public order" in linea):
+            linea=linea.replace('"Public order"', '10')
+        if("Robbery" in linea):
+            linea=linea.replace('"Robbery"', '11')
+        if("Shoplifting" in linea):
+            linea=linea.replace('"Shoplifting"', '12')
+        if("Theft from the person" in linea):
+            linea=linea.replace('"Theft from the person"', '13')
+        if("Vehicle crime" in linea):
+            linea=linea.replace('"Vehicle crime"', '14')     
+        if("Violence and sexual offences" in linea):
+            linea=linea.replace('"Violence and sexual offences"', '15')  
+        if("Violent crime" in linea):
+            linea=linea.replace('"Violent crime"', '16')
+
         #transformo vector de string en cadena y automatica elimino corchetes
-        cad=",".join(row)
-        f.write(cad+'\n')
-    
+        #cad=",".join(row)
+        
+        f.write(linea)
+        linea=Acsv.readline()
+
     #cerrar el del csv
     Acsv.close()
     #cerrar el que escribe f
@@ -68,3 +75,6 @@ def creandobigfile(nombreFusionar):
     b = datetime.now()
     delta = b - a
     print 'Tiempo Proceso: '+str(delta.seconds)+ ' segundos'
+
+
+creandobigfile(path)
